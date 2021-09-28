a2e821365b2a9d3152a1bef9ffc3c713bf3b433a
Parth Patel <parth6796@yahoo.com>
2021-09-28 11:53:44 +0100

    added app.py controller reference

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug import exceptions
+from puns import puns
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,17 @@ CORS(app)
 @app.route("/")
 def welcome():
     return "Welcome to Gunanas - The Home of Fruits Puns"
+
+
+@app.route("/puns", methods=["GET", "POST"])
+def puns_handler():
+    fns = {"GET": puns.show, "POST": puns.create}
+    return fns[request.method](request)
+
+
+@app.route("/puns/<int:id>", methods=["GET"])
+def index_puns(id):
+    return puns.index(id)
 
 
 if __name__ == "__main__":
