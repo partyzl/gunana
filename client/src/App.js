dd6f7eecc83bfa63c90d4b56dc46708fd88ee058
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-09-28 16:44:07 +0100

    form styles

--- client/src/App.js
+++ client/src/App.js
@@ -9,28 +9,27 @@ function App() {
   const [isFormExpanded, setFormExpanded] = useState(false);
   const [puns, setPuns] = useState([]);
 
-    const [isFormExpanded, setFormExpanded] = useState(false)
-    const [puns, setPuns] = useState([])
+  useEffect(async () => {
+    const url = "http://localhost:5000/puns";
+    const resp = await fetch(url);
+    const json = await resp.json();
+    console.log(json);
+    // assume resp is fine
+    setPuns(json);
+  }, []);
 
-    useEffect(async () => {
-        const url = 'http://localhost:5000/puns'
-        const resp = await fetch(url)
-        const json = await resp.json()
-        console.log(json)
-        // assume resp is fine
-        setPuns(json)
-    }, [])
-
-    useEffect(async () => {
-        console.log('puns changed', puns)
-    }, [puns])
+  useEffect(async () => {
+    console.log("puns changed", puns);
+  }, [puns]);
 
   return (
     <div className="App">
       <header className="App-header">
-        { isFormExpanded ?
-        <AddPunForm setFormExpanded={setFormExpanded} setPuns={setPuns}/> :
-        <AddPunButton setFormExpanded={setFormExpanded}/> }
+        {isFormExpanded ? (
+          <AddPunForm setFormExpanded={setFormExpanded} setPuns={setPuns} />
+        ) : (
+          <AddPunButton setFormExpanded={setFormExpanded} />
+        )}
       </header>
       <main>
         <PunCardList puns={puns} />
