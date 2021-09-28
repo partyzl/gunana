6334544d37d3cc785446a11b55653e39296c201e
Parth Patel <parth6796@yahoo.com>
2021-09-28 17:15:36 +0100

    netlify

--- server/test/test_app.py
+++ server/test/test_app.py
@@ -8,3 +8,21 @@ def test_api_get_puns(api):
         "name": "Abi",
         "pun": "test punny2",
     }
+
+
+def test_api_get_pun(api):
+    res = api.get("/puns/1")
+    assert res.json["puns"]["name"] == "Abi"
+
+
+def test_api_post_puns(api):
+    mock_data = json.dumps({"name": "Owen"})
+    mock_headers = {"Content-Type": "application/json"}
+    res = api.post("/puns", data=mock_data, headers=mock_headers)
+    assert res.json["puns"]["id"] == 3
+
+
+def test_api_not_found(api):
+    res = api.get("/bob")
+    assert res.status == "404 NOT FOUND"
+    assert "Oops!" in res.json["message"]
